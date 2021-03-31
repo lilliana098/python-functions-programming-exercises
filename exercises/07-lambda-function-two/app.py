0ae507d8180402a90c24f3925e20561d1dc32b22
lilliana098 <lillianacc@uned.cr>
2021-03-31 00:47:13 +0000

    Completados

--- exercises/07-lambda-function-two/app.py
+++ exercises/07-lambda-function-two/app.py
@@ -1,5 +1,4 @@
-
-
+rapid= lambda str: str[:-1]
 
 # From this line above, plese do not change code below
 print(rapid("bob")) #should print bo
--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,5 +1,6 @@
 # Your code goes here:
-def render_person(param):
+def render_person(name, date_born, eye_color, age, gender):
+    param = name + " is a "+ str(age)+ " years old "+ gender + " born in "+ date_born + " with "+eye_color+" eyes"
     return param
 
 
